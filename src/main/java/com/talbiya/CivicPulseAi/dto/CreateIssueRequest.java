50d7eabe5a5cbdf32c4659559c1faa7be0dc8043
TALBIYA-PARVEEN <2k24.cs1q.2413756@gmail.com>
2026-06-25 11:58:55 +0530

    Issue Request dto created

---
+++ src/main/java/com/talbiya/CivicPulseAi/dto/CreateIssueRequest.java
@@ -0,0 +1,18 @@
+package com.talbiya.CivicPulseAi.dto;
+
+import com.talbiya.CivicPulseAi.enums.IssueCategory;
+import lombok.Data;
+
+@Data
+public class CreateIssueRequest {
+
+    private String title;
+
+    private String description;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private IssueCategory category;
+}
---
+++ src/main/java/com/talbiya/CivicPulseAi/dto/IssueResponse.java
@@ -0,0 +1,29 @@
+package com.talbiya.CivicPulseAi.dto;
+
+import com.talbiya.CivicPulseAi.enums.IssueCategory;
+import com.talbiya.CivicPulseAi.enums.IssueStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class IssueResponse {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private IssueCategory category;
+
+    private IssueStatus status;
+
+    private LocalDateTime createdAt;
+}
